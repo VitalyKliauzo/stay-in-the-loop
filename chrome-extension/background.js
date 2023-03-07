7f515af21dc52c627b8dd9270a900d0b499a7cff
Vitali Kliauzo <vitalykliauzo@coherentsolutions.com>
2023-03-07 22:37:24 +0100

    chrome extension

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,11 +1,41 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
+// chrome.tabs.onUpdated.addListener((tabId, tab) => {
+//     if (tab.url && tab.url.includes("youtube.com/watch")) {
+//       const queryParameters = tab.url.split("?")[1];
+//       const urlParameters = new URLSearchParams(queryParameters);
   
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
-});
+//       chrome.tabs.sendMessage(tabId, {
+//         type: "NEW",
+//         videoId: urlParameters.get("v"),
+//       });
+//     }
+// });
+
+// function updateBatteryLevel(level, isCharging) {
+//     const batteryLevelText = level !== 1 ? (level * 100).toFixed() : '';
+//     const chargingStatus = isCharging ? 'charging' : 'not-charging';
+  
+//     chrome.browserAction.setIcon({
+//       path: `./images/icon-${ chargingStatus }.png`
+//     });
+  
+//     chrome.browserAction.setBadgeText({
+//       text: batteryLevelText
+//     });
+  
+//     chrome.browserAction.setBadgeBackgroundColor({
+//       color: [94, 97, 106, 255]
+//     });
+//   }
+  
+//   function getBatteryStatus() {
+//     navigator.getBattery().then(battery => {
+//       updateBatteryLevel(battery.level, battery.charging);
+//     });
+//   }
+  
+//   chrome.alarms.create('batteryLevel', {
+//     when: Date.now() + 1000,
+//     periodInMinutes: 2
+//   });
+//   chrome.runtime.onInstalled.addListener(getBatteryStatus);
+//   chrome.alarms.onAlarm.addListener(getBatteryStatus);
--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,12 +1,26 @@
-// adding a new bookmark row to the popup
-const addNewBookmark = () => {};
-
-const viewBookmarks = () => {};
-
-const onPlay = e => {};
-
-const onDelete = e => {};
-
-const setBookmarkAttributes =  () => {};
-
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", (event) => {
+    navigator.getBattery()
+        .then(batteryManager => {
+            batteryManager.onlevelchange = (event) => {
+                const level = event.currentTarget.level * 100;
+                const charge = level <= 20;
+                const unplug = level >= 80;
+                const charging = event.currentTarget.charging;
+                chrome.action.setBadgeText({
+                    text: event.currentTarget.level !== 1 ? level.toString() : ''
+                });
+                //if (!charging && charge || charging && unplug) {
+                    chrome.notifications.create('NOTFICATION_ID', {
+                        type: 'basic',
+                        iconUrl: 'images/icon-48.png',
+                        title: 'Charge Battery Message',
+                        message: charge ? 'CHARGE ME' : 'UNPLUG ME',
+                        priority: 2
+                    })
+               // }
+            }
+        })
+});
+document.addEventListener('unload', (event) => {
+    alert('unload');
+ });
