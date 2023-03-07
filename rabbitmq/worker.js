15c73b244181b0967f0ccc463453794745c1c49d
Vitali Kliauzo <vitalykliauzo@coherentsolutions.com>
2023-03-07 22:38:43 +0100

    rabbitmq

---
+++ rabbitmq/worker.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(error, connection) {
+    connection.createChannel(function(error, channel) {
+        var queue = 'task_queue';
+
+        channel.assertQueue(queue, {
+            durable: true
+        });
+        channel.prefetch(1);
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+        channel.consume(queue, function(msg) {
+            var secs = msg.content.toString().split('.').length - 1;
+            console.log(msg.content.toString(), secs);
+
+            console.log(" [x] Received %s", msg.content.toString());
+            setTimeout(function() {
+                console.log(" [x] Done");
+                channel.ack(msg);
+            }, secs * 1000);
+        }, {
+            noAck: false
+        });
+    });
+});
